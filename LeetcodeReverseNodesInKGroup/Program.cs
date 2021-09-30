6b14c4c07c54112e1d132c5b6b94b8578f06a8ac
Scott Tunstall <scott.tunstall@ntlworld.com>
2021-09-30 20:46:08 +0100

    First attempt. Initial checkin for safekeeping. Haven't run this in leetcode yet.

---
+++ LeetcodeReverseNodesInKGroup/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetcodeReverseNodesInKGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // head = [1,2,3,4,5], k = 3
+
+            var nodes = new ListNode(1,
+                new ListNode(2,
+                    new ListNode(3,
+                        new ListNode(4,
+                            new ListNode(5)))));
+
+            var solution = new Solution();
+            var result =  solution.ReverseKGroup(nodes, 2);
+
+
+        }
+    }
+}
---
+++ LeetcodeReverseNodesInKGroup/Solution.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeReverseNodesInKGroup
+{
+    public class Solution
+    {
+
+
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            if (head == null)
+                return null;
+
+            if (k == 0)
+                return head;
+
+            if (!TryReverseKNodes(head, k, out var newHead, out var newTail))
+                return head;
+
+            var current = newTail;
+
+            while (current != null)
+            {
+                if (!TryReverseKNodes(current.next, k, out var headOfSubsetOfNodes, out newTail))
+                    break;
+
+                current.next = headOfSubsetOfNodes;
+                current = newTail;
+            }
+
+            return newHead;
+        }
+
+
+        private bool TryReverseKNodes(ListNode head, int k, out ListNode newHead, out ListNode newTail)
+        {
+            var listNodes = new Stack<ListNode>();
+            var current = head;
+            var i = 0;
+
+            while (current != null && i < k)
+            {
+                listNodes.Push(current);
+                current = current.next;
+                i++;
+            }
+
+            if (listNodes.Count !=k)
+            {
+                newHead = head;
+                newTail = null;
+                return false;
+            }
+            
+            var nodeAfterOldTail  = current;
+
+            var headOfReversedNodes = listNodes.Pop();
+
+            current = headOfReversedNodes;
+            while (listNodes.TryPop(out ListNode listNode))
+            {
+                current.next = listNode;
+                current = listNode;
+            }
+
+            current.next = nodeAfterOldTail;
+
+            newHead = headOfReversedNodes;
+            newTail = current;
+
+            return true;
+        }
+    }
+}
